1719ea492887da896f25d97b9109d1abf05c39e4
TessHutchinson <165493472+TessHutchinson@users.noreply.github.com>
2024-07-11 22:22:55 -0700

    Initial commit

---
+++ app/WorkoutScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const WorkoutScreen = () => {
+  return (
+    <View>
+      <Text>Your workout recommendations will appear here.</Text>
+    </View>
+  );
+};
+
+export default WorkoutScreen;
---
+++ renderer.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const title = document.getElementById('title');
+    title.innerText = "Welcome to FitnessApp";
+  });
+  
